e6b82bde5e77f439885b45981eb70477ac025eb1
Anchit Mittal <anchit.mittal@kohls.com>
2020-07-01 16:48:08 +0530

    Added Dummy Login Feature

--- app/src/main/java/com/learn/samplecicd/MainActivity.kt
+++ app/src/main/java/com/learn/samplecicd/MainActivity.kt
@@ -16,26 +16,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun init() {
 
-        val email=et_id.text.trim().toString()
-        val password=et_password.text.trim().toString()
 
         btn_submit.setOnClickListener {
+            val email = et_id.text.trim().toString()
+            val password = et_password.text.trim().toString()
 
+            if (!TextUtils.isEmpty(password) && !TextUtils.isEmpty(password) && password == "0000") {
 
-            if(!TextUtils.isEmpty(password) && !TextUtils.isEmpty(password) && password=="0000"){
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            } else {
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show()
+                } else {
+                    if (TextUtils.isEmpty(password)) {
+                        Toast.makeText(this, "Enter PIN", Toast.LENGTH_SHORT).show()
+                    } else {
 
-                Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show()
-            }else{
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(this,"Enter Email",Toast.LENGTH_SHORT).show()
-                }
-                if(TextUtils.isEmpty(password)){
-                    Toast.makeText(this,"Enter PIN",Toast.LENGTH_SHORT).show()
+                        if (password != "0000") {
+                            Toast.makeText(this, "Invalid PIN", Toast.LENGTH_SHORT).show()
+                        }
+                    }
                 }
             }
-
-
-
 
 
         }
